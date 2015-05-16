0c8f8e3aed49945f40c4cd00b926637285d20a56
Matthias Noback <matthiasnoback@gmail.com>
2015-05-16 17:10:29 +0200

    Initial situation

--- run.php
+++ run.php
@@ -2,22 +2,9 @@
 
 require __DIR__  . '/vendor/autoload.php';
 
-use CatApi\CatApi;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Workshop\ContainerFactory;
-
-$factory = new ContainerFactory(__DIR__ . '/cache', __DIR__ . '/config');
-
-$container = $factory->create(function(ContainerBuilder $container, LoaderInterface $loader) {
-    // load any configuration file you like
-    $loader->load('services.yml');
-
-    // maybe register a compiler pass
-    //$container->addCompilerPass(...);
-});
+use ImageGenerator\CatApi;
+use ImageGenerator\MemeGeneratorApi;
 
 $catApi = new CatApi();
 
-echo 'URL for cat gif with id "vd": ' . $catApi->getCatGifUrl('vd') . "\n";
-echo 'A random URL of a cat gif: ' . $catApi->getRandomCatGifUrl() . "\n";
+echo 'A random cat image: ' . $catApi->getRandomImage() . "\n";
