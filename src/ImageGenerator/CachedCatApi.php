85c5bd5e71622a5cbd72d7e85ea68c23b7360bc8
Matthias Noback <matthiasnoback@gmail.com>
2015-05-19 12:29:30 +0200

    Extract CatApiCache class

--- src/ImageGenerator/CachedCatApi.php
+++ src/ImageGenerator/CachedCatApi.php
@@ -6,16 +6,15 @@ class CachedCatApi
 {
     public function getRandomImage()
     {
-        if (!file_exists(__DIR__ . '/../../cache/random') || time() - filemtime(__DIR__ . '/../../cache/random') > 3) {
+        $cache = new CatApiCache();
+
+        if ($cache->containsNoRandomImage()) {
             $catApi = new CatApi();
             $url = $catApi->getRandomImage();
-            file_put_contents(
-                __DIR__ . '/../../cache/random',
-                $url
-            );
+            $cache->storeRandomImage($url);
             return $url;
         } else {
-            return file_get_contents(__DIR__ . '/../../cache/random');
+            return $cache->retrieveRandomImage();
         }
     }
 }
---
+++ src/ImageGenerator/CatApiCache.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ImageGenerator;
+
+class CatApiCache
+{
+    public function containsNoRandomImage()
+    {
+        return !file_exists(__DIR__ . '/../../cache/random') || time() - filemtime(__DIR__ . '/../../cache/random') > 3;
+    }
+
+    public function storeRandomImage($url)
+    {
+        file_put_contents(
+            __DIR__ . '/../../cache/random',
+            $url
+        );
+    }
+
+    public function retrieveRandomImage()
+    {
+        return file_get_contents(__DIR__ . '/../../cache/random');
+    }
+}
